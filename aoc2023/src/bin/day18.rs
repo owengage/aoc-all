eb376f3defad788de2e2ba648dbd564e73422bf9
Owen Gage <owengage@gmail.com>
2023-12-20 10:03:02 +0000

    day18

--- aoc2023/src/bin/day18.rs
+++ aoc2023/src/bin/day18.rs
@@ -1,135 +1,159 @@
-use std::{
-    collections::VecDeque,
-    fmt::{Display, Write},
-};
+use core::panic;
+use std::collections::HashMap;
 
 use aoc::{
     lines,
-    two::{pt, DenseField, Dirn, IPoint},
+    two::{pt, Dirn, IPoint, DOWN, LEFT, RIGHT, UP},
 };
 use itertools::Itertools;
 
-#[derive(Debug, Clone)]
-enum Cell {
-    Trench { colour: String },
-    Empty { flooded: bool },
-}
-impl Display for Cell {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Cell::Trench { .. } => f.write_char('#'),
-            Cell::Empty { flooded: true } => f.write_char('o'),
-            Cell::Empty { flooded: false } => f.write_char('.'),
-        }
-    }
-}
 fn main() {
-    let input = lines("aoc2023/input/work18");
+    // let input = lines("input/work18");
+    let input = lines("aoc2023/input/day18");
     let instructions = parse_instructions(input);
-    let bounds = find_bounds(&instructions);
-    let width = bounds.1.x - bounds.0.x;
-    let height = bounds.1.y - bounds.0.y;
-    let start = pt(-bounds.0.x + 2, -bounds.0.y + 2); // ?
-    let mut field = DenseField::new(width + 4, height + 4, Cell::Empty { flooded: false });
-
-    dig_trench(start, &instructions, &mut field);
-
-    let inside_point = find_inside_point(&field);
-
-    flood(inside_point, &mut field);
-
-    let part1 = part1(&field);
+    let part1 = sparse_fill(&instructions);
     dbg!(part1);
+
+    let new_instructions = reparse_instructions(&instructions);
+    let part2 = sparse_fill(&new_instructions);
+
+    dbg!(part2);
 }
 
-fn part1(field: &DenseField<Cell>) -> usize {
-    let mut count = 0;
-    for p in field.points() {
-        match field.get(p.x, p.y) {
-            Cell::Trench { .. } => count += 1,
-            Cell::Empty { flooded: true } => count += 1,
-            Cell::Empty { flooded: false } => {}
+fn reparse_instructions(instructions: &[Instr]) -> Vec<Instr> {
+    let mut new_ins = vec![];
+
+    for ins in instructions {
+        let data = ins.colour.strip_prefix('#').unwrap();
+        let dist = isize::from_str_radix(&data[0..5], 16).unwrap();
+        let dirn = match data.chars().last().unwrap() {
+            '0' => RIGHT,
+            '1' => DOWN,
+            '2' => LEFT,
+            '3' => UP,
+            _ => panic!(),
         };
+
+        new_ins.push(Instr {
+            distance: dist,
+            dirn,
+            colour: String::new(),
+        })
     }
 
-    count
+    new_ins
 }
 
-fn flood(start: IPoint, field: &mut DenseField<Cell>) {
-    let mut q = VecDeque::new();
-    q.push_back(start);
+#[allow(clippy::nonminimal_bool)]
+fn sparse_fill(instructions: &[Instr]) -> usize {
+    let verts = to_verts(instructions);
+    let horz = to_horz(instructions);
 
-    while let Some(p) = q.pop_front() {
-        if let Cell::Empty { flooded: false } = field.get_mut(p.x, p.y) {
-            *field.get_mut(p.x, p.y) = Cell::Empty { flooded: true };
-        } else {
-            continue;
-        }
+    let mut filled = 0;
 
-        for (next_cell, next_p) in field.neighbours8_bounded(p.x, p.y) {
-            if let Cell::Empty { flooded: false } = next_cell {
-                q.push_back(next_p);
-            }
-        }
-    }
-}
+    let (top, bottom) = verts.iter().fold((0, 0), |acc, line| {
+        (acc.0.min(line.0.y), acc.1.max(line.0.y + line.1))
+    });
 
-fn find_inside_point(field: &DenseField<Cell>) -> IPoint {
-    for y in 0..field.height() {
-        let mut trench_count = 0;
-        for x in 0..field.width() {
-            let cell = field.get(x, y);
-            match cell {
-                Cell::Trench { .. } => trench_count += 1,
-                Cell::Empty { .. } => {
-                    if trench_count == 1 {
-                        return pt(x, y);
-                    }
+    for row in top..bottom + 1 {
+        let relevant = verts
+            .iter()
+            .filter(|line| (line.0.y..line.0.y + line.1 + 1).contains(&row))
+            .collect_vec();
 
-                    if trench_count > 1 {
-                        break;
+        let mut in_trench = false;
+
+        let count: isize = relevant
+            .windows(2)
+            .map(|chunk| {
+                let left = chunk[0].0.x;
+                let right = chunk[1].0.x;
+
+                let left_top = chunk[0].0.y;
+                let left_bottom = chunk[0].0.y + chunk[0].1;
+                let right_top = chunk[1].0.y;
+                let right_bottom = chunk[1].0.y + chunk[1].1;
+
+                let left_crosses = left_top != row;
+
+                if left_crosses {
+                    in_trench = !in_trench;
+                }
+
+                if (left_top == right_top && left_top == row)
+                    || (left_top == right_bottom && left_top == row)
+                    || (left_bottom == right_top && left_bottom == row)
+                    || (left_bottom == right_bottom && left_bottom == row)
+                {
+                    if let Some(hor) = horz.get(&row) {
+                        for l in hor {
+                            if left == l.0.x {
+                                return right - left;
+                            }
+                        }
                     }
                 }
-            }
-        }
+
+                if in_trench {
+                    right - left
+                } else {
+                    1
+                }
+            })
+            .sum();
+
+        // We don't include the last trench above per row, so need to add
+        // one here.
+        filled += count + 1;
     }
-    todo!()
+
+    filled as usize
 }
 
-fn dig_trench(start: IPoint, instructions: &[Instr], field: &mut DenseField<Cell>) {
-    let mut current = start;
-
-    for ins in instructions {
-        for d in 0..ins.distance {
-            let delta = current + ins.dirn * d;
-            let cell = field.get_mut(delta.x, delta.y);
-            assert!(delta == start || matches!(cell, Cell::Empty { .. }));
-
-            *cell = Cell::Trench {
-                colour: ins.colour.clone(),
-            };
-        }
-        current += ins.dirn * ins.distance;
-    }
-}
-
-fn find_bounds(instructions: &[Instr]) -> (IPoint, IPoint) {
-    let mut left = 0;
-    let mut right = 0;
-    let mut top = 0;
-    let mut bottom = 0;
+fn to_verts(instructions: &[Instr]) -> Vec<(IPoint, isize)> {
     let mut current = pt(0, 0);
+    let mut lines = vec![];
 
     for ins in instructions {
+        match ins.dirn.y {
+            -1 => lines.push((current + ins.dirn * ins.distance, ins.distance)),
+            1 => lines.push((current, ins.distance)),
+            0 => {}
+            _ => panic!(),
+        }
         current += ins.dirn * ins.distance;
-
-        left = left.min(current.x);
-        right = right.max(current.x);
-        top = top.min(current.y);
-        bottom = bottom.max(current.y);
     }
 
-    (pt(left, top), pt(right, bottom))
+    lines.sort_by_key(|(p, _d)| p.x);
+    lines
+}
+
+// Key for hashmap is the **Y** coordinate of the horizontals.
+fn to_horz(instructions: &[Instr]) -> HashMap<isize, Vec<(IPoint, isize)>> {
+    let mut current = pt(0, 0);
+    let mut lines = HashMap::<isize, Vec<_>>::new();
+
+    for ins in instructions {
+        match ins.dirn.x {
+            -1 => lines
+                .entry(current.y)
+                .or_default()
+                .push((current + ins.dirn * ins.distance, ins.distance)),
+            1 => lines
+                .entry(current.y)
+                .or_default()
+                .push((current, ins.distance)),
+            0 => {}
+            _ => panic!(),
+        }
+        current += ins.dirn * ins.distance;
+    }
+
+    for v in lines.values_mut() {
+        v.sort_by_key(|(p, _d)| p.x);
+    }
+
+    lines
 }
 
 #[derive(Debug)]
@@ -160,7 +184,116 @@ fn parse_instructions(input: Vec<String>) -> Vec<Instr> {
 
 #[cfg(test)]
 mod test {
+    use aoc::lines_from_str;
+
+    use crate::*;
 
     #[test]
-    fn test_parse() {}
+    fn square() {
+        let input = r#"R 4 (#123456)
+D 3 (#123456)
+L 4 (#123456)
+U 3 (#123456)"#;
+        let input = lines_from_str(input);
+        let instructions = parse_instructions(input);
+        let count = sparse_fill(&instructions);
+        assert_eq!(20, count);
+    }
+
+    #[test]
+    fn lshape() {
+        let input = r#"R 4 (#123456)
+D 3 (#123456)
+R 4 (#123456)
+D 3 (#123456)
+L 8 (#123456)
+U 6 (#123456)"#;
+        let input = lines_from_str(input);
+        let instructions = parse_instructions(input);
+
+        let count = sparse_fill(&instructions);
+        assert_eq!(36 + 15, count);
+    }
+
+    #[test]
+    fn nshape() {
+        let input = r#"R 12 (#123456)
+D 6 (#123456)
+L 3 (#123456)
+U 3 (#123456)
+L 6 (#123456)
+D 3 (#123456)
+L 3 (#123456)
+U 6 (#123456)"#;
+        let input = lines_from_str(input);
+        let instructions = parse_instructions(input);
+
+        let count = sparse_fill(&instructions);
+        assert_eq!(12 + 12 + 4 * 13, count);
+    }
+
+    #[test]
+    fn up_l() {
+        let input = r#"R 4 (#123456)
+U 4 (#123456)
+R 4 (#123456)
+D 8 (#123456)
+L 8 (#123456)
+U 4 (#123456)
+"#;
+        let input = lines_from_str(input);
+        let instructions = parse_instructions(input);
+
+        let count = sparse_fill(&instructions);
+        assert_eq!(20 + 5 * 9, count);
+    }
+
+    #[test]
+    fn example() {
+        let input = r#"R 6 (#70c710)
+D 5 (#0dc571)
+L 2 (#5713f0)
+D 2 (#d2c081)
+R 2 (#59c680)
+D 2 (#411b91)
+L 5 (#8ceee2)
+U 2 (#caa173)
+L 1 (#1b58a2)
+U 2 (#caa171)
+R 2 (#7807d2)
+U 3 (#a77fa3)
+L 2 (#015232)
+U 2 (#7a21e3)"#;
+        let input = lines_from_str(input);
+        let instructions = parse_instructions(input);
+
+        let count = sparse_fill(&instructions);
+        assert_eq!(62, count);
+    }
+
+    #[test]
+    fn saw() {
+        let input = r#"D 6 (#70c710)
+R 2 (#70c710)
+U 3 (#70c710)
+R 2 (#70c710)
+D 3 (#70c710)
+R 2 (#70c710)
+U 3 (#70c710)
+R 2 (#70c710)
+D 3 (#70c710)
+R 2 (#70c710)
+U 3 (#70c710)
+R 2 (#70c710)
+D 3 (#70c710)
+R 2 (#70c710)
+U 6 (#70c710)
+L 14 (#70c710)"#;
+
+        let input = lines_from_str(input);
+        let instructions = parse_instructions(input);
+
+        let count = sparse_fill(&instructions);
+        assert_eq!(15 * 7 - 9, count);
+    }
 }
