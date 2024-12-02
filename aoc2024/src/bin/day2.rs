67378214c28bfd2e14478ae9fbe972f41c554d63
Owen Gage <owengage@gmail.com>
2024-12-02 14:25:25 +0000

    day2

--- aoc2024/src/bin/day2.rs
+++ aoc2024/src/bin/day2.rs
@@ -1,1 +1,62 @@
-fn main() {}
+use aoc::lines;
+
+fn main() {
+    let input = lines("input/day2");
+    let reports: Vec<_> = input
+        .into_iter()
+        .map(|line| {
+            let vals: Vec<usize> = line
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect();
+            vals
+        })
+        .collect();
+
+    let part1: usize = reports
+        .iter()
+        .map(|report| fully_safe(report) as usize)
+        .sum();
+
+    dbg!(part1);
+
+    let part2: usize = reports
+        .iter()
+        .map(|report| {
+            if fully_safe(report) {
+                return 1;
+            }
+            for i in 0..report.len() {
+                let mut sub = report.clone();
+                sub.remove(i);
+
+                if fully_safe(&sub) {
+                    return 1;
+                }
+            }
+            0
+        })
+        .sum();
+
+    dbg!(part2);
+}
+
+fn fully_safe(report: &[usize]) -> bool {
+    let mut up = false;
+    let mut down = false;
+    let mut diffsafe = true;
+    report.windows(2).for_each(|w| {
+        if w[0] < w[1] {
+            up = true;
+        }
+        if w[0] > w[1] {
+            down = true;
+        }
+        let diff = w[0].abs_diff(w[1]);
+        if diff > 3 || diff == 0 {
+            diffsafe = false;
+        }
+    });
+
+    up ^ down && diffsafe
+}
