460444e3b69675e08d5153f958b1c4191b605bfb
Owen Gage <owengage@gmail.com>
2025-12-01 14:31:02 +0000

    2025d1

---
+++ aoc2025/src/bin/day1.rs
@@ -0,0 +1,50 @@
+use aoc::{fetch_input, lines};
+
+fn main() {
+    let input = lines(fetch_input(2025, 1));
+    let mut dial = 50; // starts at 50.
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    for mut line in input {
+        let mut num: isize = line.split_off(1).parse().unwrap();
+        let dir = line;
+
+        part2 += num / 100;
+        num = num % 100;
+
+        if dial != 0 && num != 0 {
+            match dir.as_str() {
+                "L" => {
+                    if dial - num <= 0 {
+                        part2 += 1
+                    }
+                }
+                "R" => {
+                    if dial + num >= 100 {
+                        part2 += 1;
+                    }
+                }
+                _ => panic!(),
+            }
+        }
+
+        // Update the dial
+        match dir.as_str() {
+            "L" => dial -= num,
+            "R" => dial += num,
+            _ => panic!("{}, {}", dir, num),
+        }
+        dial = ((dial % 100) + 100) % 100;
+
+        if dial == 0 {
+            part1 += 1;
+        }
+    }
+
+    println!("part1 = {}", part1);
+    println!("part2 = {}", part2);
+
+    assert_eq!(part1, 1092);
+    assert_eq!(part2, 6616);
+}
