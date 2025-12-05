30c4b1c1d29d4cc6333e8ead55f88073e5d2cc4e
Owen Gage <owengage@gmail.com>
2025-12-05 09:49:09 +0000

    Switch 2025d5 to RangeInclusive rather than tuple.

--- aoc2025/src/bin/day5.rs
+++ aoc2025/src/bin/day5.rs
@@ -1,3 +1,5 @@
+use std::ops::RangeInclusive;
+
 use aoc::{StrExt, fetch_input, line_blocks};
 use itertools::Itertools;
 
@@ -6,33 +8,34 @@ fn main() {
     let ranges = input[0]
         .iter()
         .map(|range| range.trim().split_once_parse::<usize>("-"))
+        .map(|(low, high)| low..=high)
         .collect_vec();
     let ids: Vec<usize> = input[1].iter().map(|id| id.parse().unwrap()).collect();
 
     let part1 = part1(&ranges, &ids);
     println!("part1 = {part1}");
-    // assert_eq!(756, part1);
+    assert_eq!(756, part1);
 
     let part2 = part2(&ranges);
     println!("part2 = {part2}");
-    // assert_eq!(355555479253787, part2);
+    assert_eq!(355555479253787, part2);
 }
 
-fn part2(ranges: &[(usize, usize)]) -> usize {
+fn part2(ranges: &[RangeInclusive<usize>]) -> usize {
     let mut ranges = ranges.to_vec();
-    ranges.sort_by_key(|r| r.0); // sort by lower bound.
+    ranges.sort_by_key(|r| *r.start()); // sort by lower bound.
 
     let mut new_ranges = vec![];
-    let mut current = ranges[0];
+    let mut current = ranges[0].clone();
 
     for next in &ranges[1..] {
-        if next.0 > current.1 {
+        if next.start() > current.end() {
             // we're beyond the end of the current range.
             new_ranges.push(current);
-            current = *next;
+            current = next.clone();
         } else {
             // absorb next range.
-            current = (current.0, current.1.max(next.1));
+            current = (*current.start())..=(*current.end().max(next.end()));
         }
     }
 
@@ -41,22 +44,22 @@ fn part2(ranges: &[(usize, usize)]) -> usize {
 
     // Sanity check
     for rs in ranges.windows(2) {
-        assert!(rs[0].1 < rs[1].0);
+        assert!(rs[0].end() < rs[1].start());
     }
 
     let mut sum = 0;
     for r in ranges {
-        sum += 1 + r.1 - r.0;
+        sum += 1 + r.end() - r.start();
     }
 
     sum
 }
 
-fn part1(ranges: &[(usize, usize)], ids: &[usize]) -> usize {
+fn part1(ranges: &[RangeInclusive<usize>], ids: &[usize]) -> usize {
     let mut count = 0;
     'outer: for id in ids {
         for range in ranges {
-            if (range.0..=range.1).contains(&id) {
+            if (range.start()..=range.end()).contains(&id) {
                 count += 1;
                 continue 'outer;
             }
