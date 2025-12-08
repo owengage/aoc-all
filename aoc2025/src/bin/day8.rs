be68f7952253f6a0b2efb7db73253bc855067aa2
Owen Gage <owengage@gmail.com>
2025-12-08 10:43:26 +0000

    Tidy.

--- aoc2025/src/bin/day8.rs
+++ aoc2025/src/bin/day8.rs
@@ -1,5 +1,5 @@
 use core::panic;
-use std::collections::HashSet;
+use std::{collections::HashSet, mem};
 
 use aoc::{StrExt, fetch_input, lines};
 use itertools::Itertools;
@@ -35,20 +35,20 @@ fn main() {
         })
         .collect_vec();
 
-    let part1 = part1(coords.clone(), 1000);
+    let pairs = make_pairs(&coords);
+    let circuits = init_circuits(&coords);
+
+    let part1 = part1(&pairs, circuits.clone());
     println!("part1 = {part1}");
     assert_eq!(122636, part1);
 
-    let part2 = part2(coords);
+    let part2 = part2(&pairs, circuits);
     println!("part2 = {part2}");
     assert_eq!(9271575747, part2);
 }
 
-fn part1(coords: Vec<Vec3>, connections_to_make: usize) -> usize {
-    let pairs = make_pairs(&coords);
-    let mut circuits = Vec::<HashSet<Vec3>>::new();
-
-    for pair in pairs.iter().take(connections_to_make) {
+fn part1(pairs: &Vec<(Vec3, Vec3)>, mut circuits: Vec<HashSet<Vec3>>) -> usize {
+    for pair in pairs.iter().take(1000) {
         connect_pair(&mut circuits, *pair);
     }
 
@@ -56,26 +56,26 @@ fn part1(coords: Vec<Vec3>, connections_to_make: usize) -> usize {
     circuits.iter().rev().map(|c| c.len()).take(3).product()
 }
 
-fn part2(coords: Vec<Vec3>) -> usize {
-    let pairs = make_pairs(&coords);
-    let mut circuits = Vec::<HashSet<Vec3>>::new();
-
+fn part2(pairs: &Vec<(Vec3, Vec3)>, mut circuits: Vec<HashSet<Vec3>>) -> usize {
+    let coord_count = circuits.len();
     for pair in pairs {
-        connect_pair(&mut circuits, pair);
+        connect_pair(&mut circuits, *pair);
 
-        // We've potentially just made a connection. Are we now fully connected?
-        if circuits[0].len() == coords.len() {
-            // Which junction boxes made this happen?
-            let jb0 = pair.0;
-            let jb1 = pair.1;
-            return jb0.x as usize * jb1.x as usize;
+        // Are we now fully connected?
+        if circuits[0].len() == coord_count {
+            return pair.0.x as usize * pair.1.x as usize;
         }
     }
 
     panic!("didn't fully connect");
 }
 
-// Make all pairs of coordinates, sorted from shortest distance to longest.
+/// Make a circuit for each coordinate.
+fn init_circuits(coords: &Vec<Vec3>) -> Vec<HashSet<Vec3>> {
+    Vec::<HashSet<Vec3>>::from_iter(coords.iter().map(|c| HashSet::from_iter([*c])))
+}
+
+/// Make all pairs of coordinates, sorted from shortest distance to longest.
 fn make_pairs(coords: &[Vec3]) -> Vec<(Vec3, Vec3)> {
     let pairs = coords
         .iter()
@@ -92,86 +92,22 @@ fn make_pairs(coords: &[Vec3]) -> Vec<(Vec3, Vec3)> {
 
 // Given the existing circuits, connect the given pair if necessary.
 fn connect_pair(circuits: &mut Vec<HashSet<Vec3>>, pair: (Vec3, Vec3)) {
-    let p0in = circuits
-        .iter()
-        .find_position(|c| c.iter().contains(&pair.0));
-    let p1in = circuits
-        .iter()
-        .find_position(|c| c.iter().contains(&pair.1));
+    let i0 = find_circuit(circuits, pair.0);
+    let i1 = find_circuit(circuits, pair.1);
 
-    match (p0in, p1in) {
-        (None, None) => {
-            // neither are in a circuit, so make one.
-            let c = HashSet::from_iter([pair.0, pair.1]);
-            circuits.push(c);
-        }
-        (None, Some((i, _))) => {
-            // one is in a circuit.
-            circuits.get_mut(i).unwrap().insert(pair.0);
-        }
-        (Some((i, _)), None) => {
-            // one is in a circuit.
-            circuits.get_mut(i).unwrap().insert(pair.1);
-        }
-        (Some((i0, _)), Some((i1, _))) if i0 == i1 => {
-            // in the same circuit already!
-        }
-        (Some((i0, _)), Some((i1, _))) => {
-            // in different circuits
-            let c1 = circuits.get(i1).unwrap().clone();
-            let c0 = circuits.get_mut(i0).unwrap();
-            for v in c1 {
-                c0.insert(v);
-            }
-            circuits.remove(i1);
-        }
+    // join different circuits
+    if i0 != i1 {
+        let stolen = mem::take(circuits.get_mut(i1).unwrap());
+        circuits.get_mut(i0).unwrap().extend(stolen);
+        circuits.remove(i1);
     }
 }
 
-#[cfg(test)]
-mod test {
-    use aoc::StrExt;
-    use itertools::Itertools;
-
-    use crate::{Vec3, part1};
-
-    #[test]
-    fn example() {
-        let input = "162,817,812
-57,618,57
-906,360,560
-592,479,940
-352,342,300
-466,668,158
-542,29,236
-431,825,988
-739,650,466
-52,470,668
-216,146,977
-819,987,18
-117,168,530
-805,96,715
-346,949,466
-970,615,88
-941,993,340
-862,61,35
-984,92,344
-425,690,689";
-        let coords = input
-            .lines()
-            .into_iter()
-            .map(|s| {
-                let mut it = s.split_parse::<isize>(",");
-                Vec3 {
-                    x: it.next().unwrap(),
-                    y: it.next().unwrap(),
-                    z: it.next().unwrap(),
-                }
-            })
-            .collect_vec();
-
-        println!("{:?}", coords);
-
-        assert_eq!(40, part1(coords, 10));
-    }
+// Find index of circuit that coord is in.
+fn find_circuit(circuits: &mut Vec<HashSet<Vec3>>, coord: Vec3) -> usize {
+    circuits
+        .iter()
+        .find_position(|c| c.contains(&coord))
+        .unwrap()
+        .0
 }
